46725addd74cf957128d16d09fe37f8d1563894f
sobitp59 <sobitp59@gmail.com>
2023-07-22 17:01:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+/*
+  ::HEADER
+    :Logo
+    :SearchBar
+    :Profile
+    :Theme
+
+  ::BODY
+    :SIDE-LEFT
+      -HOME (BTN)
+      -EXPLORE (BTN)
+      -BOOKMARKS (BTN)
+      -LIKED POSTS (BTN)
+      -POST (BTN)
+    
+    :MAIN
+      -POST SECTION
+    
+    :SIDE-RIGHT
+      -Trending / Latest
+      -Suggestions
+
+  ::FOOTER
+    :COPYRIGHT
+    : OTHER INFO
+*/
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>social media app</h1>
     </div>
-  );
+    );
 }
 
 export default App;
